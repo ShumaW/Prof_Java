e80a9dd440094c6d66f8a521737e147d8334c088
Yevgeniy Shulimenko <evgeniy.shulimenko@gmail.com>
2022-12-20 18:05:06 +0100

    change

--- Homework_20221219_j/src/Main.java
+++ Homework_20221219_j/src/Main.java
@@ -5,17 +5,31 @@ public class Main {
 
         OurLinkedList<Integer> list = new OurLinkedList<>();
         list.append(5);
+        System.out.println("-".repeat(25) + " print element " + "-".repeat(25));
         System.out.println(list.get(0));
         System.out.println(list.size());
         list.append(15);
         list.append(2);
+        System.out.println("-".repeat(25) + " add elements " + "-".repeat(25));
         list.append(10);
         list.append(117);
         list.append(20);
         list.append(30);
+        System.out.println("-".repeat(25) + " print element " + "-".repeat(25));
         for (Integer el : list) {
             System.out.println(el);
         }
+
+        System.out.println("-".repeat(25) + " remove each odd element in the list " + "-".repeat(25));
+        for (Integer el : list) {
+            if (el%2 == 0) list.remove(el);
+        }
+
+        for (Integer el : list) {
+            System.out.println(el);
+        }
+
+
 
     }
 }
