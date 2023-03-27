87658a8da34b7fe3f9a50e3c90fa4a586c2dbd86
Yevgeniy Shulimenko <evgeniy.shulimenko@gmail.com>
2023-03-27 14:36:07 +0200

    Change HW 20.03.2023

--- HW_20230320_j/src/main/java/org/example/Main.java
+++ HW_20230320_j/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.*;
 
@@ -24,10 +26,10 @@ public class Main {
         Random random = new Random();
 
         @Override
-        public Integer call(){
+        public Integer call() {
             System.out.println("House building number ");
             try {
-                Thread.sleep(2000 + random.nextInt(10));
+                Thread.sleep(10000 + random.nextInt(10000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -36,52 +38,31 @@ public class Main {
             for (int i = 0; i < countOfOperation; i++) {
                 sum += random.nextInt(50000);
             }
-            System.out.println(sum);
+            System.out.println("Price for house is: " + sum);
             return sum;
         }
     }
+
     private static int countBuilding = 10;
 
     public static void main(String[] args) {
-        Integer totalPrice = 0;
+        int totalPrice = 0;
 
-        for (int i = 0; i < countBuilding; i++) {
-            // Если строительство будет вести только одна бригада, достаточно переписать Executors.newSingleThreadExecutor()
-            // Если бригады будут создаваться по количеству строящихся домов, а после распускаться, достаточно у Executors.newFixedThreadPool указать количество домов. В данном случае 4 заменить на счётчик countBuilding.
-            ExecutorService building = Executors.newFixedThreadPool(4);
-            try {
-                Future<Integer> future1 = building.submit(new House());
-                totalPrice += future1.get();
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
+        ExecutorService building = Executors.newFixedThreadPool(4);
+
+        try {
+            List<Future<Integer>> futuresList = building.invokeAll(Arrays.asList(new House(), new House(), new House(), new House(), new House(), new House(), new House(), new House(), new House(), new House()));
+
+            for (int i = 0; i < futuresList.size(); i++) {
+
+                totalPrice = totalPrice + futuresList.get(i).get();
+
             }
-            building.shutdown();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
+        building.shutdown();
 
         System.out.println("The final price of " + countBuilding + " building is: " + totalPrice);
     }
 }
-
-
-
-//static class House1 implements Runnable {
-//
-//        int countOfOperation = 15;
-//        Random random = new Random();
-//
-//        @Override
-//        public void run() {
-//            System.out.println("House building number ");
-//            try {
-//                Thread.sleep(10000 + random.nextInt(10));
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println("House finished");
-//            int sum = 0;
-//            for (int i = 0; i < countOfOperation; i++) {
-//                sum += random.nextInt(50000);
-//            }
-//            System.out.println(sum);
-//        }
-//    }
