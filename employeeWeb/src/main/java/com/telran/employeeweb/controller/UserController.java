95612f2ed8fb9c2bad29ff48fb49d50f80abfbe4
Yevgeniy Shulimenko <evgeniy.shulimenko@gmail.com>
2023-04-10 20:15:05 +0200

    HW 05.04.2023

--- employeeWeb/src/main/java/com/telran/employeeweb/controller/UserController.java
+++ employeeWeb/src/main/java/com/telran/employeeweb/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -22,14 +22,14 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public String getAll(Model model) {
         List<User> users = userService.getUsers();
         model.addAttribute("users", users);
         return "users";
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public String getById(Model model, @PathVariable String id) {
         User userById = userService.getUserById(id);
         model.addAttribute("userById", userById);
